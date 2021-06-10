a36339f50319cf679c1412f36511282ac649da90
Varela <guilhermevarela@protonmail.com>
2021-06-10 15:49:11 +0100

    Update dwmblocks directories for GNU/stow

--- sources/dwmblocks/blocks.def.h
+++ sources/dwmblocks/blocks.def.h
@@ -1,9 +1,9 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"",            "$HOME/.local/bin/dwmblocks/dwmresources",				     1,		        0},
-	{"",            "$HOME/.local/bin/dwmblocks/dwmaudio",				         1,		        0},
-	{"",            "$HOME/.local/bin/dwmblocks/dwmdatetime",				     1,		        0},
+	{"",            "$BIN_HOME/scripts/dwmresources",				     1,		        0},
+	{"",            "$BIN_HOME/scripts/dwmaudio",				         1,		        0},
+	{"",            "$BIN_HOME/scripts/dwmdatetime",				     1,		        0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
