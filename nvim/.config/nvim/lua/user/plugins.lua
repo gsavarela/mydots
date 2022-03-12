9814cf578e35621fef1b8a717e56612bda311a45
Guilherme Varela <guilhermevarela@protonmail.com>
2022-03-12 13:05:27 +0000

    Add gitsigns plugin: for git integration.
    References:
    -----------
    * https://github.com/lewis6991/gitsigns.nvim
    * https://github.com/LunarVim/Neovim-from-scratch/tree/11-gitsigns

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -81,6 +81,8 @@ return packer.startup(function(use)
    -- Comments
   use 'numToStr/Comment.nvim' -- Easily comment stuff
   use 'JoosepAlviste/nvim-ts-context-commentstring'
+  -- Git
+  use 'lewis6991/gitsigns.nvim'
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
