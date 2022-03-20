2852fb5ede7492f0aec00596ccde8297978664fd
Guilherme Varela <guilhermevarela@protonmail.com>
2022-03-20 15:00:47 +0000

    Add whichkey plugin: command completion suggestion
    References:
    -----------
    * https://github.com/LunarVim/Neovim-from-scratch/tree/21-whichkey
    * https://github.com/folke/which-key.nvim

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -101,6 +101,9 @@ return packer.startup(function(use)
 
   -- toggleterm
   use 'akinsho/toggleterm.nvim'
+
+  -- which key
+  use 'folke/which-key.nvim'
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
