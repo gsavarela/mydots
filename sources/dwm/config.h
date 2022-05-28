d26dc12b7006b38587e6f4ea14e375cc6263ad84
Guilherme Varela <guilhermevarela@protonmail.com>
2022-05-28 15:52:06 +0100

    Add dwm
    Copy original config.def.h
    Patches
    -------
    patches/dwm-systray-6.3.diff
    patches/dwm-systray-dwmblocks-6.3.diff
    patches/dwm-fullgaps-6.2.diff

--- sources/dwm/config.h
+++ sources/dwm/config.h
@@ -1,16 +1,22 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;     /* 0 means no systray */
+static const int showbar            = 1;     /* 0 means no bar */
+static const int topbar             = 1;     /* 0 means bottom bar */
 /* FONTS */
 static const char *fonts[]          = { "monospace:size=12" };
 static const char dmenufont[]       = "monospace:size=12";
 
 /* COLORSCHEMES */
+
+enum { SchemeNorm, SchemeSel};
 static const char normbgcolor[]       = "#282828";
 static const char normbordercolor[]   = "#504945";
 static const char normfgcolor[]       = "#ebdbb2";
@@ -22,6 +28,32 @@ static const char *colors[][3]      = {
 	[SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
 	[SchemeSel]  = { selfgcolor, selbgcolor,  selbordercolor  },
 };
+// static const char col_gray1[]       = "#222222";
+// static const char col_gray2[]       = "#444444";
+// static const char col_gray3[]       = "#bbbbbb";
+// static const char col_gray4[]       = "#eeeeee";
+// static const char col_cyan[]        = "#005577";
+// static const char col1[]            = "#ffffff";
+// static const char col2[]            = "#ffffff";
+// static const char col3[]            = "#ffffff";
+// static const char col4[]            = "#ffffff";
+// static const char col5[]            = "#ffffff";
+// static const char col6[]            = "#ffffff";
+//
+// enum { SchemeNorm, SchemeCol1, SchemeCol2, SchemeCol3, SchemeCol4,
+//        SchemeCol5, SchemeCol6, SchemeSel }; /* color schemes */
+//
+// static const char *colors[][3]      = {
+// 	/*               fg         bg         border   */
+// 	[SchemeNorm]  = { col_gray3, col_gray1, col_gray2 },
+// 	[SchemeCol1]  = { col1,      col_gray1, col_gray2 },
+// 	[SchemeCol2]  = { col2,      col_gray1, col_gray2 },
+// 	[SchemeCol3]  = { col3,      col_gray1, col_gray2 },
+// 	[SchemeCol4]  = { col4,      col_gray1, col_gray2 },
+// 	[SchemeCol5]  = { col5,      col_gray1, col_gray2 },
+// 	[SchemeCol6]  = { col6,      col_gray1, col_gray2 },
+// 	[SchemeSel]   = { col_gray4, col_cyan,  col_cyan  },
+// };
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
@@ -62,11 +94,53 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+// static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
+
 #include "shiftview.c"
 #include <X11/XF86keysym.h>
+// static Key keys[] = {
+// 	/* modifier                     key        function        argument */
+// 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+// 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+// 	{ MODKEY,                       XK_b,      togglebar,      {0} },
+// 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+// 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+// 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+// 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+// 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+// 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+// 	{ MODKEY,                       XK_Return, zoom,           {0} },
+// 	{ MODKEY,                       XK_Tab,    view,           {0} },
+// 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+// 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+// 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+// 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+// 	{ MODKEY,                       XK_space,  setlayout,      {0} },
+// 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+// 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+// 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+// 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
+// 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
+// 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
+// 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+// 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
+// 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
+// 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
+// 	TAGKEYS(                        XK_1,                      0)
+// 	TAGKEYS(                        XK_2,                      1)
+// 	TAGKEYS(                        XK_3,                      2)
+// 	TAGKEYS(                        XK_4,                      3)
+// 	TAGKEYS(                        XK_5,                      4)
+// 	TAGKEYS(                        XK_6,                      5)
+// 	TAGKEYS(                        XK_7,                      6)
+// 	TAGKEYS(                        XK_8,                      7)
+// 	TAGKEYS(                        XK_9,                      8)
+// 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+// };
+
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
@@ -116,6 +190,26 @@ static Key keys[] = {
     { 0, XF86XK_AudioLowerVolume,   spawn,          SHCMD("pamixer -d 5") },
     { 0, XF86XK_AudioMute,          spawn,          SHCMD("pamixer -t") },
 };
+
+/* button definitions */
+/* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
+// static Button buttons[] = {
+// 	/* click                event mask      button          function        argument */
+// 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+// 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+// 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
+// 	{ ClkStatusText,        0,              Button1,        sigdwmblocks,   {.i = 1} },
+// 	{ ClkStatusText,        0,              Button2,        sigdwmblocks,   {.i = 2} },
+// 	{ ClkStatusText,        0,              Button3,        sigdwmblocks,   {.i = 3} },
+// 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
+// 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
+// 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+// 	{ ClkTagBar,            0,              Button1,        view,           {0} },
+// 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
+// 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+// 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+// };
+//
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
