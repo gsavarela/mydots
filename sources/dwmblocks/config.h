c78b3b04b2ce7973e96cc5355f3e4b0e4931b9ad
Guilherme Varela <guilhermevarela@protonmail.com>
2022-06-02 15:22:03 +0100

    Customize dwmblocks-async and dwm volume controls

--- sources/dwmblocks/config.h
+++ sources/dwmblocks/config.h
@@ -3,9 +3,9 @@
 #define CLICKABLE_BLOCKS
 
 const Block blocks[] = {
-	BLOCK("sb-disk",    1800, 19),
-	BLOCK("sb-memory",  10,   20),
-	BLOCK("sb-volume",  0,    22),
-	BLOCK("sb-battery", 5,    23),
-	BLOCK("sb-date",    1,    24)
+	BLOCK("sb-cpubars",    5,   3),
+	BLOCK("sb-memory",    10,   4),
+	BLOCK("sb-volume",     0,   5),
+	BLOCK("sb-battery",    5,   6),
+	BLOCK("sb-clock",      1,   7)
 };
