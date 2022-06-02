dc9652560f60bcde4e590ed5f2e5242798d6e943
Guilherme Varela <guilhermevarela@protonmail.com>
2022-06-02 14:05:11 +0100

    Switch vanilla dwmblocks with dwmblocks-async

---
+++ sources/dwmblocks/config.h
@@ -0,0 +1,11 @@
+#define CMDLENGTH 45
+#define DELIMITER "  "
+#define CLICKABLE_BLOCKS
+
+const Block blocks[] = {
+	BLOCK("sb-disk",    1800, 19),
+	BLOCK("sb-memory",  10,   20),
+	BLOCK("sb-volume",  0,    22),
+	BLOCK("sb-battery", 5,    23),
+	BLOCK("sb-date",    1,    24)
+};
